5c2fd5fe736eb15aedee6406e6710ba4d24e021b
Barry Scott Rollan <b.scott.rollan@gmail.com>
2019-03-06 14:38:01 -0500

    heroku tester

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path')
-const MONGODB_URI = require('./config/keys.js')
+// const MONGODB_URI = require('./config/keys.js')
 
 const PORT = process.env.PORT || 3304;
 const app = express();
@@ -10,12 +10,12 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname,'./public')))
-
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/kudos'
 //Set up promises with mongoose
 mongoose.Promise = global.Promise;
 //Connect to the Mongo DB
 mongoose.connect(
-    MONGODB_URI || 'mongodb://localhost/kudos', {useNewUrlParser: true});
+    MONGODB_URI, {useNewUrlParser: true});
 
 require ('./routes/api-routes')(app);
 require ('./routes/html-routes')(app);
